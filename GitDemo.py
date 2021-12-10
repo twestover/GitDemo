80e59f9d860f0b8e2cb731ae4d4a37b05eb0de26
Tom Westover <32076511+twestover@users.noreply.github.com>
2021-12-10 15:44:47 -0800

    Initial Code Base upload

---
+++ GitDemo.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 10 15:44:14 2021
+
+@author: twestover
+"""
+
+# %% Dependencies
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# %%
+
+# Set a variable
+a = 34
+
+# compute a second value
+b = 2 * a + 69
+
+# report
+
+print(b)
+
